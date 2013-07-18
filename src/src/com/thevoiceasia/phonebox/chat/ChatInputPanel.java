17417af0ff6937072c14447763804beba5d96dee
administrator <administrator@uknas001.(none)>
2013-07-18 13:01:56 +0100

    XMPP Chat Module, Initial commit

---
+++ src/src/com/thevoiceasia/phonebox/chat/ChatInputPanel.java
@@ -0,0 +1,13 @@
+package com.thevoiceasia.phonebox.chat;
+
+import javax.swing.JPanel;
+
+public class ChatInputPanel extends JPanel {
+
+	/*
+	 * Panel for chat input, possibly buttons for shortcuts
+	 */
+	
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/src/com/thevoiceasia/phonebox/chat/ConversationPanel.java
@@ -0,0 +1,21 @@
+package com.thevoiceasia.phonebox.chat;
+
+import javax.swing.JPanel;
+
+public class ConversationPanel extends JPanel {
+	
+	/*
+	 * Panel for showing conversation so far
+	 */
+	
+	public void addMessage(){
+		
+		/*
+		 * Function to add message to conversation panel
+		 */
+		
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
