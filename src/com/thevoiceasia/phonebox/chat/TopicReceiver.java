c91981eb5e5b64b1c53eb9c51997aafdc5cf9cd8
Wayne Merricks <waynemerricks@thevoiceasia.com>
2013-07-19 18:52:47 +0100

    Added Topic Receiver Interface

---
+++ src/com/thevoiceasia/phonebox/chat/TopicReceiver.java
@@ -0,0 +1,16 @@
+package com.thevoiceasia.phonebox.chat;
+
+/**
+ * TODO
+ * @author waynemerricks
+ *
+ */
+public interface TopicReceiver {
+
+	/**
+	 * TODO
+	 * @param topic
+	 */
+	public void receiveTopic(String topic);
+	
+}
