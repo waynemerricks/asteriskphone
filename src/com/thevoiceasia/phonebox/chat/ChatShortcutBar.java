c31805aa90f6b858170b184d724b63881c9ca26a
Wayne Merricks <waynemerricks@thevoiceasia.com>
2013-07-19 13:05:50 +0100

    Initial Skeleton for chat shortcut bar providing things like "No Calls please"

---
+++ src/com/thevoiceasia/phonebox/chat/ChatShortcutBar.java
@@ -0,0 +1,26 @@
+package com.thevoiceasia.phonebox.chat;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class ChatShortcutBar extends JPanel implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ChatShortcutBar(){
+		
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
