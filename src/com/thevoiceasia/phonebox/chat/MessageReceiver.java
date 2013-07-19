1480d73a8e19c8355e51a0726631e72f5b37b2f5
Wayne Merricks <waynemerricks@thevoiceasia.com>
2013-07-19 18:51:34 +0100

    Added missing comments to class

--- src/com/thevoiceasia/phonebox/chat/MessageReceiver.java
+++ src/com/thevoiceasia/phonebox/chat/MessageReceiver.java
@@ -2,9 +2,17 @@ package com.thevoiceasia.phonebox.chat;
 
 import org.jivesoftware.smack.packet.Message;
 
+/**
+ * TODO
+ * @author waynemerricks
+ *
+ */
 public interface MessageReceiver {
 
-	
+	/**
+	 * TODO
+	 * @param msg
+	 */
 	public void receiveMessage(Message msg);
 	
 }
