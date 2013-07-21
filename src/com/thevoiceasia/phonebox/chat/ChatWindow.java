f0f21962fd6cdda75bbe1a055069216fe67115d3
Wayne Merricks <waynemerricks@thevoiceasia.com>
2013-07-21 02:56:53 +0100

    Added mouse listeners to provide a toggle for online list panel

--- src/com/thevoiceasia/phonebox/chat/ChatWindow.java
+++ src/com/thevoiceasia/phonebox/chat/ChatWindow.java
@@ -1,13 +1,32 @@
 package com.thevoiceasia.phonebox.chat;
 
 import java.awt.BorderLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JPanel;
 
-public class ChatWindow extends JPanel {
+/**
+ * Panel containing all the XMPP Chat Components
+ * @author Wayne Merricks
+ *
+ */
+public class ChatWindow extends JPanel implements MouseListener {
 
+	/** CLASS VARS **/
+	private UserStatusPanel userStatus;
+	private boolean statusVisible = false;
+	
 	/** STATICS **/
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * Makes a JPanel containing all the chat components.
+	 * @param chatManager
+	 * @param language
+	 * @param country
+	 * @param myNickName
+	 */
 	public ChatWindow(ChatManager chatManager, String language, String country, String myNickName){
 		
 		super();
@@ -29,14 +48,65 @@ public class ChatWindow extends JPanel {
 		chatManager.connect();
 		chatManager.getChatRoom().addMessageListener(messages);//Messages needs to know about new message packets
 		chatManager.getChatRoom().addSubjectUpdatedListener(messages);//Messages also needs to know about topic changes
+		chatManager.getChatRoom().addParticipantStatusListener(messages);//And when people are joining/leaving
 		messages.subjectUpdated(chatManager.getChatRoom().getSubject(), "");//Send the current topic to messages //$NON-NLS-1$
-
+		messages.getTextPane().addMouseListener(this);
+		
 		this.setLayout(new BorderLayout());
 		this.add(new ChatShortcutBar(language, country, chatManager.getChatRoom()), BorderLayout.NORTH);
 		this.add(messages, BorderLayout.CENTER);
 		this.add(new ChatInputPanel(language, country, chatManager.getChatRoom()), BorderLayout.SOUTH);
-		this.add(new UserStatusPanel(), BorderLayout.EAST);
+		
+		userStatus = new UserStatusPanel(language, country, chatManager.getChatRoom());
+		chatManager.getChatRoom().addParticipantStatusListener(userStatus);
+		chatManager.getChatRoom().addParticipantListener(userStatus);
+		userStatus.getTextPane().addMouseListener(new MouseListener(){
+			public void mouseClicked(MouseEvent evt){
+				hideUserStatus();
+			}
+
+			public void mouseEntered(MouseEvent e) { }
+			public void mouseExited(MouseEvent e) { }
+			public void mousePressed(MouseEvent e) { }
+			public void mouseReleased(MouseEvent e) { }
+			
+		});
 		
 	}
+
+	private void showUserStatus(){
+		this.add(userStatus, BorderLayout.EAST);
+		statusVisible = true;
+		validate();
+	}
+	
+	private void hideUserStatus(){
+		this.remove(userStatus);
+		statusVisible = false;
+		validate();
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		
+		if(!statusVisible){
+			showUserStatus();
+		}else{
+			hideUserStatus();
+		}
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {}
 
 }
