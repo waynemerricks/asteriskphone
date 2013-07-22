8709017723e5083db7f38907b1a969df93f3963b
Wayne Merricks <waynemerricks@thevoiceasia.com>
2013-07-22 01:40:43 +0100

    Added DB lookups for settings.  More to come.

--- src/com/thevoiceasia/phonebox/gui/Client.java
+++ src/com/thevoiceasia/phonebox/gui/Client.java
@@ -3,7 +3,9 @@ package com.thevoiceasia.phonebox.gui;
 import java.awt.BorderLayout;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,7 +14,7 @@ import javax.swing.*;
 
 import com.thevoiceasia.phonebox.chat.ChatManager;
 import com.thevoiceasia.phonebox.chat.ChatWindow;
-import com.thevoiceasia.phonebox.chat.I18NStrings;
+import com.thevoiceasia.phonebox.database.DatabaseManager;
 
 import javax.swing.UIManager.*;
 
@@ -20,32 +22,85 @@ public class Client extends JFrame implements WindowListener{
 
 	//Class Vars
 	private ChatManager chatManager;
-	private static I18NStrings xStrings;
+	private DatabaseManager databaseManager;
+	private String language, country;
+	private boolean hasErrors = false;
+	private HashMap<String, String> userSettings;
 	
 	//Statics
 	private static final Logger LOGGER = Logger.getLogger(Client.class.getName());//Logger
 	private static final Level LOG_LEVEL = Level.INFO;
+	private static I18NStrings xStrings;
 	
-	public Client(){
+	public Client(String language, String country){
 		
 		super();
 		
+		//Set Locale
+		this.language = language;
+		this.country = country;
+		
 		/** Initialise component daemons **/
-		//TODO replace settings with db lookups
+		xStrings = new I18NStrings(language, country);
 		setupLogging();
-		xStrings = new I18NStrings("en", "GB");
-		chatManager = new ChatManager("newssms@elastix", "N3wssmsas1a", "News SMS", "elastix", "phonebox@conference.elastix", "en", "GB");
-		//TESTING
-		/** Build GUI **/
-		setLookandFeel();
-		this.setSize(340, 400);
-		this.setLayout(new BorderLayout());
-		this.setTitle(xStrings.getString("Client.appTitle")); //$NON-NLS-1$
-		this.addWindowListener(this);
-		
-		this.add(new ChatWindow(chatManager, "en", "GB", "News SMS"), BorderLayout.CENTER);
-		
+		setupManagementObjects();
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		if(!hasErrors){
+			
+			/** Build GUI **/
+			setLookandFeel();
+			this.setSize(340, 400);
+			this.setLayout(new BorderLayout());
+			this.setTitle(xStrings.getString("Client.appTitle")); //$NON-NLS-1$
+			this.addWindowListener(this);
+			
+			//Chat Module
+			this.add(new ChatWindow(chatManager, language, country, userSettings.get("nickName")), BorderLayout.CENTER); //$NON-NLS-1$
+			
+			if(chatManager.hasErrors())
+				hasErrors = true;
+		
+		}
+		
+	}
+	
+	private void setupManagementObjects(){
+		
+		//Database
+		databaseManager = new DatabaseManager(new File("settings/settings.conf"), language, country); //$NON-NLS-1$
+		
+		if(databaseManager.hasErrors())
+			hasErrors = true;
+		else{
+			
+			databaseManager.connect();
+			
+			if(!databaseManager.hasErrors()){
+				
+				databaseManager.populateUserSettings();
+				userSettings = databaseManager.getUserSettings();
+			
+				//Chat Connection Manager
+				chatManager = new ChatManager(userSettings.get("XMPPLogin") + "@" +  //$NON-NLS-1$ //$NON-NLS-2$
+						userSettings.get("XMPPDomain"), //$NON-NLS-1$ 
+						userSettings.get("password"), //$NON-NLS-1$
+						userSettings.get("nickName"), //$NON-NLS-1$
+						userSettings.get("XMPPServer"), //$NON-NLS-1$
+						userSettings.get("XMPPRoom"), //$NON-NLS-1$
+						userSettings.get("language"), //$NON-NLS-1$
+						userSettings.get("country")); //$NON-NLS-1$
+				
+				if(chatManager.hasErrors())
+					hasErrors = true;
+				else{
+					//Asterisk Manager TODO
+				}
+				
+			}
+			
+		}
 		
 	}
 	
@@ -65,44 +120,50 @@ public class Client extends JFrame implements WindowListener{
 		
 	}
 	
-	public boolean hasErrors(){
+	private boolean hasErrors(){
 		
-		return chatManager.hasErrors();
-		
-	}
-	
-	private static Exception getLoadException(){
-		
-		return new Exception(xStrings.getString("Client.onLoadError"));
+		return hasErrors;
 		
 	}
-	
 	
 	public static void main(String[] args){
 		
-		Client phonebox = new Client();
 		
-		if(!phonebox.hasErrors())
-			phonebox.setVisible(true);
-		else{
-			Exception e = getLoadException();
-			showError(e, e.getMessage());
+		
+		if(args.length == 2){
+			
+			I18NStrings xStrings = new I18NStrings(args[0], args[1]);
+			Client phonebox = new Client(args[0], args[1]);
+		
+			if(!phonebox.hasErrors())
+				phonebox.setVisible(true);
+			else{
+				
+				Exception e = new Exception(xStrings.getString("Client.onLoadError")); //$NON-NLS-1$
+
+				System.err.println(xStrings.getString("Client.errorPrefix") + e.getMessage()); //$NON-NLS-1$
+				e.printStackTrace();
+				JOptionPane.showMessageDialog(null, e.getMessage(), xStrings.getString("Client.errorBoxTitle"), JOptionPane.ERROR_MESSAGE); //$NON-NLS-1$
+				LOGGER.severe(e.getMessage());
+				
+				System.exit(1);
+				
+			}
+			
+		}else{
+			
+			I18NStrings xStrings = new I18NStrings("en", "GB");  //$NON-NLS-1$//$NON-NLS-2$
+			Exception e = new Exception(xStrings.getString("Client.usageError")); //$NON-NLS-1$
+
+			System.err.println(xStrings.getString("Client.errorPrefix") + e.getMessage()); //$NON-NLS-1$
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, e.getMessage(), xStrings.getString("Client.errorBoxTitle"), JOptionPane.ERROR_MESSAGE); //$NON-NLS-1$
+			LOGGER.severe(e.getMessage());
+			
 			System.exit(1);
+			
 		}
-		
-	}
-	
-	/**
-	 * Logs an error message and displays friendly message to user
-	 * @param e
-	 * @param friendlyErrorMessage
-	 */
-	private static void showError(Exception e, String friendlyErrorMessage){
-		
-		System.err.println(xStrings.getString("ChatManager.errorPrefix") + friendlyErrorMessage); //$NON-NLS-1$
-		e.printStackTrace();
-		JOptionPane.showMessageDialog(null, friendlyErrorMessage, xStrings.getString("ChatManager.errorBoxTitle"), JOptionPane.ERROR_MESSAGE); //$NON-NLS-1$
-		LOGGER.severe(friendlyErrorMessage);
+			
 		
 	}
 	
