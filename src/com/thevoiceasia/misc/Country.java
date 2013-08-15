e51c6555084c8303d0f9266e72ccde08f3cea607
Wayne Merricks <waynemerricks@thevoiceasia.com>
2013-08-15 16:39:33 +0100

    Added helper classes for phone number location lookups

---
+++ src/com/thevoiceasia/misc/Country.java
@@ -0,0 +1,25 @@
+package com.thevoiceasia.misc;
+
+public class Country {
+
+	private String name, ISO, phone;
+	
+	public Country(String name, String ISOCode, String phonePrefix){
+		this.name = name;
+		this.ISO = ISOCode;
+		this.phone = phonePrefix;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getISOCode(){
+		return ISO;
+	}
+	
+	public String getPhoneCode(){
+		return phone;
+	}
+	
+}
