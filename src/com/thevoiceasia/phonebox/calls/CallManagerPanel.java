d62796113d52887c19c677791f9fef6b05888706
Wayne Merricks <waynemerricks@thevoiceasia.com>
2013-08-19 18:33:53 +0100

    Changed to require database manager Added: isStudioExtension, checks if this extension belongs to a studio or not Added: populateStudioExtensions, reads studio extension numbers from the DB so you can check whether a call is potentially on air or not when answered BUG FIX: timezoneOffset in CallInfoPanel, see CallInfoPanel for more info

--- src/com/thevoiceasia/phonebox/calls/CallManagerPanel.java
+++ src/com/thevoiceasia/phonebox/calls/CallManagerPanel.java
@@ -1,6 +1,5 @@
 package com.thevoiceasia.phonebox.calls;
 
-import java.sql.Connection;
 import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,6 +17,7 @@ import org.jivesoftware.smack.packet.Packet;
 import org.jivesoftware.smackx.muc.MultiUserChat;
 
 import com.thevoiceasia.misc.CountryCodes;
+import com.thevoiceasia.phonebox.database.DatabaseManager;
 
 public class CallManagerPanel extends JPanel implements PacketListener{
 
@@ -42,8 +42,9 @@ public class CallManagerPanel extends JPanel implements PacketListener{
 	private I18NStrings xStrings;
 	private CountryCodes countries;
 	private HashMap<String, CallInfoPanel> callPanels = new HashMap<String, CallInfoPanel>();
-	private Connection databaseReadConnection;
+	private DatabaseManager database;
 	private HashMap<String, String> settings;
+	private HashMap<String, String> studioExtensions = new HashMap<String, String>();
 	
 	/* We need to spawn threads for event response with db lookups, in order to guard against
 	 * craziness, we'll use the ExecutorService to have X threads available to use (set via
@@ -53,7 +54,7 @@ public class CallManagerPanel extends JPanel implements PacketListener{
 	private int maxExecutorThreads;
 	
 	public CallManagerPanel(HashMap<String, String> settings, MultiUserChat controlRoom, 
-			Connection databaseReadConnection){
+			DatabaseManager database){
 		
 		this.controlRoom = controlRoom;
 		this.controlRoom.addMessageListener(this);
@@ -63,19 +64,77 @@ public class CallManagerPanel extends JPanel implements PacketListener{
 		xStrings = new I18NStrings(settings.get("language"), settings.get("country")); //$NON-NLS-1$ //$NON-NLS-2$
 		maxExecutorThreads = Integer.parseInt(settings.get("threadPoolMax")); //$NON-NLS-1$
 		dbLookUpService = Executors.newFixedThreadPool(maxExecutorThreads);
-		this.databaseReadConnection = databaseReadConnection;
+		this.database = database;
+		
+		populateStudioExtensions();
+				
 		this.setLayout(new MigLayout(new LC().fillX()));
 		
 	}
 	
+	/**
+	 * Internal method, reads studioExtensions from db and separates them into a hashmap
+	 * [extension] => [name]
+	 */
+	private void populateStudioExtensions(){
+		
+		//Parse Studio Extensions
+		if(settings.get("studioExtensions") != null){ //$NON-NLS-1$
+			
+			if(settings.get("studioExtensions").contains(",")){  //$NON-NLS-1$//$NON-NLS-2$
+				
+				String[] studios = settings.get("studioExtensions").split(","); //$NON-NLS-1$ //$NON-NLS-2$
+				
+				for(int i = 0; i < studios.length; i++){
+				
+					if(studios[i].contains("=>")){ //$NON-NLS-1$
+						
+						String[] temp = studios[i].split("=>"); //$NON-NLS-1$
+						
+						if(temp.length == 2)
+							studioExtensions.put(temp[0], temp[1]);
+						
+					}
+					
+				}
+				
+			}else if(settings.get("studioExtensions").contains("=>")){  //$NON-NLS-1$//$NON-NLS-2$
+			
+				String[] temp = settings.get("studioExtensions").split("=>"); //$NON-NLS-1$ //$NON-NLS-2$
+				
+				if(temp.length == 2)
+					studioExtensions.put(temp[0], temp[1]);
+				
+			}
+				
+		}
+		
+	}
+	
+	/* TODO Build up an extensions to names list
+	 * by listening to commands from other users
+	 * hashmap it extension, name, overwrite existing if name is different (same computer
+	 * but different user)
+	 */
+	/* TODO Alternate proposal
+	 * Get user list from chat room
+	 * Read username --> myExtension from DB
+	 * Need to update when people join or leave
+	 */
+	/* TODO MyExtension is per machine not per username.  Would have to rewrite settings
+	 * to lookup machine extension and then write in username -> extension for others
+	 * In popsettings would have to ignore username -> extension
+	 */
 	/**
 	 * Helper method to create a call info panel and then spawn a thread to grab details
 	 * from the DB (via standard Executor)
 	 * @param phoneNumber phoneNumber of the call
 	 * @param channelID channel id of the call
 	 * @param mode Panel Mode to set the initial state to
+	 * @param connectedTo Name or extension this panel is connected to
 	 */
-	private void createSkeletonCallInfoPanel(String phoneNumber, String channelID, int mode){
+	private void createSkeletonCallInfoPanel(String phoneNumber, String channelID, int mode,
+			String connectedTo){
 		
 		String location = null;
 		LOGGER.info(xStrings.getString("CallManagerPanel.createSkeletonCallPanel") + //$NON-NLS-1$
@@ -95,11 +154,22 @@ public class CallManagerPanel extends JPanel implements PacketListener{
 		if(location == null)
 			location = xStrings.getString("CallManagerPanel.locationUnknown"); //$NON-NLS-1$
 		
+		int timezoneOffset = 0;
+		if(settings.get("timezoneHourOffset") != null) //$NON-NLS-1$
+			try{
+				timezoneOffset = Integer.parseInt(settings.get("timezoneHourOffset")); //$NON-NLS-1$
+			}catch(NumberFormatException e){
+				LOGGER.warning(xStrings.getString(
+						"CallManagerPanel.logErrorParsingTimezoneOffset") + //$NON-NLS-1$
+						settings.get("timezoneHourOffset")); //$NON-NLS-1$
+			}
+		
 		CallInfoPanel call = new CallInfoPanel(settings.get("language"),  //$NON-NLS-1$
 				settings.get("country"),  //$NON-NLS-1$
 				xStrings.getString("CallManagerPanel.callerUnknown"), //$NON-NLS-1$
 				location, "", CallInfoPanel.ALERT_OK, channelID,  //$NON-NLS-1$
-				false, true, controlRoom, settings.get("myExtension")); //$NON-NLS-1$
+				false, true, controlRoom, settings.get("myExtension"),//$NON-NLS-1$
+				settings.get("nickName"), timezoneOffset);//$NON-NLS-1$
 		
 		switch(mode){
 		
@@ -110,13 +180,13 @@ public class CallManagerPanel extends JPanel implements PacketListener{
 				call.setAnswered();
 				break;
 			case MODE_ANSWERED_ELSEWHERE:
-				call.setAnsweredElseWhere();
+				call.setAnsweredElseWhere(connectedTo);
 				break;
 			case MODE_QUEUED:
 				call.setQueued();
 				break;
 			case MODE_ON_AIR:
-				call.setOnAir();
+				call.setOnAir(connectedTo);
 				break;
 				
 		}
@@ -137,7 +207,23 @@ public class CallManagerPanel extends JPanel implements PacketListener{
 		
 		//Spawn thread to populate details
 		dbLookUpService.execute(
-				new InfoPanelPopulator(databaseReadConnection, call));
+				new InfoPanelPopulator(database, call, phoneNumber, channelID));
+		
+	}
+	
+	/**
+	 * Internal method that checks if the given extension belongs to a studio
+	 * @param extension
+	 * @return true if it is a studio extension
+	 */
+	private boolean isStudioExtension(String extension){
+		
+		boolean isStudio = false;
+		
+		if(studioExtensions.get(extension) != null)
+			isStudio = true;
+		
+		return isStudio;
 		
 	}
 	
@@ -177,7 +263,7 @@ public class CallManagerPanel extends JPanel implements PacketListener{
 						
 						//Create a CallInfoPanel with skeleton details
 						if(callPanels.get(command[3]) == null)
-							createSkeletonCallInfoPanel(command[1], command[3], MODE_RINGING);
+							createSkeletonCallInfoPanel(command[1], command[3], MODE_RINGING, null);
 						
 					}else if(command[0].equals(xStrings.getString("CallManagerPanel.callQueued"))){ //$NON-NLS-1$
 					
@@ -195,17 +281,17 @@ public class CallManagerPanel extends JPanel implements PacketListener{
 							
 							LOGGER.info(xStrings.getString("CallManagerPanel.CallOnAirQueue")); //$NON-NLS-1$
 							//On Air Queue
-							if(callPanels.get(command[2]) != null){
+							if(callPanels.get(command[3]) != null){
 								
 								//Already in our list so update
-								callPanels.get(command[2]).setQueued();
+								callPanels.get(command[3]).setQueued();
 								LOGGER.info(xStrings.getString("CallManagerPanel.setQueueMode")); //$NON-NLS-1$
 								
 							}else{
 								
 								//Not in our list so create skeleton and spawn update thread
 								//queue, name, number, channel
-								createSkeletonCallInfoPanel(command[2], command[3], MODE_QUEUED);
+								createSkeletonCallInfoPanel(command[2], command[3], MODE_QUEUED, null);
 								
 							}
 							
@@ -228,7 +314,23 @@ public class CallManagerPanel extends JPanel implements PacketListener{
 						 */
 						if(callPanels.get(command[3]) != null){
 							
-							callPanels.get(command[3]).setAnswered();
+							if(command[1].equals(settings.get("myExtension")) ||  //$NON-NLS-1$
+									command[2].equals(settings.get("myExtension"))) //$NON-NLS-1$
+								callPanels.get(command[3]).setAnswered();
+							else{
+								
+								if(command[1].length() > 7 || command[1].equals("5003"))//TODO DEBUG 5003 //$NON-NLS-1$
+									if(isStudioExtension(command[2])){
+										
+										callPanels.get(command[3]).setOnAir(
+												studioExtensions.get(command[2]));
+										
+									}else
+										callPanels.get(command[3]).setAnsweredElseWhere(
+												command[2]);
+								
+							}
+							
 							notifyListeners(callPanels.get(command[3]));
 							
 						}else{
@@ -238,19 +340,41 @@ public class CallManagerPanel extends JPanel implements PacketListener{
 									!command[2].equals(settings.get("myExtension"))){ //$NON-NLS-1$
 								
 								//TODO BUG?? Unknown numbers?
-								//TODO this isn't us so someone connected to someone else
-								if(command[1].length() >= 7 || command[1].equals("5003")){//TODO 5003 DEBUF //$NON-NLS-1$
+								//This isn't us so someone connected to someone else
+								if(command[1].length() >= 7 || command[1].equals("5003")){//TODO 5003 DEBUG //$NON-NLS-1$
 									
-									
+									//This is an outside call connecting to someone else
+									//Check to see if someone else = studio
+									if(!isStudioExtension(command[2]))
+										createSkeletonCallInfoPanel(command[1], command[3], 
+												CallInfoPanel.MODE_ANSWERED_ELSEWHERE, command[2]);
+									else{
+										
+										createSkeletonCallInfoPanel(command[1], command[3], 
+												CallInfoPanel.MODE_ON_AIR, 
+												studioExtensions.get(command[2]));
+										
+									}
 									
 								}
 								
-								if(command[1].length() < 7 || !command[1].equals("5003")){//TODO 5003 DEBUG //$NON-NLS-1$
+							}else{
+								
+								
+								// This is us so we're active on a call
+								if(command[1].length() >= 7 || command[1].equals("5003")){//TODO 5003 DEBUG //$NON-NLS-1$
 									
+									//This is an outside call connecting to us
+									createSkeletonCallInfoPanel(command[1], command[3], 
+											CallInfoPanel.MODE_ANSWERED, null);
 									
+									//Notify listeners that we've answered
+									notifyListeners(callPanels.get(command[3]));
+									
+									/* Not Interested in the reverse because we'll have already dealt with
+									 * it here */
 									
 								}
-								if(callerNumber.length() >= 7 || callerNumber.equals("5003"))//TODO 5003 DEBUG //$NON-NLS-1$
 								
 							}
 							
@@ -282,10 +406,13 @@ public class CallManagerPanel extends JPanel implements PacketListener{
 	
 	/**
 	 * Notifies any object listening to this CallManagerPanel
+	 * Primarily used to 
 	 * @param callInfoPanel
 	 */
 	private void notifyListeners(CallInfoPanel callInfoPanel) {
 		// TODO Auto-generated method stub
+		LOGGER.info(xStrings.getString("CallManagerPanel.notifyListeners") + //$NON-NLS-1$
+				callInfoPanel.getChannelID()); 
 		
 	}
 
