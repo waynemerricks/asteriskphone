f246c113024f35da51840e8635196b47ef07e585
Wayne Merricks <waynemerricks@thevoiceasia.com>
2013-08-21 16:55:48 +0100

    New Class: Beginnings of toolbar that interacts with CallManagerPanel.
    Propose: Answer Next, Answer Random, Drop, Dial
    
    Calls In

---
+++ src/com/thevoiceasia/phonebox/calls/CallShortcutBar.java
@@ -0,0 +1,31 @@
+package com.thevoiceasia.phonebox.calls;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+import com.thevoiceasia.phonebox.misc.LastActionTimer;
+
+public class CallShortcutBar extends JPanel implements ActionListener, LastActionTimer{
+
+	/** STATICS **/
+	private static final long serialVersionUID = 1L;
+
+	public CallShortcutBar() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public long getLastActionTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
