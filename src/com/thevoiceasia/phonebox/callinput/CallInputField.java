6f509055e2717f5ea2bbf6e1e157dde781feed98
Wayne Merricks <waynemerricks@thevoiceasia.com>
2013-09-02 18:09:31 +0100

    Added: very early days work on CallInputPanel, nothing done yet

---
+++ src/com/thevoiceasia/phonebox/callinput/CallInputField.java
@@ -0,0 +1,223 @@
+package com.thevoiceasia.phonebox.callinput;
+
+import java.awt.Component;
+import java.awt.event.ItemListener;
+import java.awt.event.KeyListener;
+import java.util.Vector;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class CallInputField {
+
+	/** STATICS */
+	public static final String TYPE_LABEL = "label"; //$NON-NLS-1$
+	public static final String TYPE_TEXT_FIELD = "textfield"; //$NON-NLS-1$
+	public static final String TYPE_TEXT_AREA = "textarea"; //$NON-NLS-1$
+	public static final String TYPE_COMBO = "combo"; //$NON-NLS-1$
+	public static final String TYPE_TAB = "tab"; //$NON-NLS-1$
+	
+	/* CLASS VARS */
+	public String name, type, tooltip, options;
+	public int id, order, parent;
+	private Component component;
+	private JLabel label;
+	
+	public CallInputField(int id, String name, String type, String tooltip, int order, 
+			int parent, String options) {
+		
+		this.id = id;
+		this.name = name;
+		this.type = type;
+		this.tooltip = tooltip;
+		this.order = order;
+		this.parent = parent;
+		this.options = options;
+		
+		createComponent();
+		
+	}
+
+	/** 
+	 * Make the component
+	 */
+	private void createComponent(){
+		
+		if(type.equals(TYPE_LABEL))
+			createLabel();
+		else if(type.equals(TYPE_TEXT_FIELD))
+			createTextField();
+		else if(type.equals(TYPE_TEXT_AREA))
+			createTextArea();
+		else if(type.equals(TYPE_COMBO))
+			createCombo();
+		
+	}
+	
+	/**
+	 * If component is a combobox will attempt to set the combobox to the given item
+	 * @param selected item you want to be selected
+	 * @return true if successful
+	 */
+	@SuppressWarnings("unchecked")
+	public void setSelected(String selected){
+		
+		if(type.equals(TYPE_COMBO))
+			((JComboBox<String>)component).setSelectedItem(selected);
+			
+	}
+	
+	/**
+	 * Gets the value of this component
+	 * @return
+	 */
+	@SuppressWarnings("unchecked") //Couldn't get around the JComboBox<String> cast warning
+	public String getValue(){
+		
+		String value = null;
+		
+		if(type.equals(TYPE_TEXT_FIELD))
+			value = ((JTextField)component).getText();
+		else if(type.equals(TYPE_TEXT_AREA))
+			value = ((JTextArea)component).getText();
+		else if(type.equals(TYPE_COMBO) && component instanceof JComboBox)
+			value = (String)((JComboBox<String>)component).getSelectedItem();
+
+		return value;
+		
+	}
+	
+	/**
+	 * Adds an item listener if this is a combobox
+	 * @param listener
+	 */
+	public void addItemListener(ItemListener listener){
+		
+		if(type.equals(TYPE_COMBO)){
+			
+			@SuppressWarnings("unchecked")
+			JComboBox<String> combo = (JComboBox<String>)component;
+			
+			combo.addItemListener(listener);
+			
+		}
+		
+	}
+	
+	/**
+	 * Adds a key listener if this is a TEXT_FIELD or TEXT_AREA
+	 * @param listener
+	 */
+	public void addKeyListener(KeyListener listener){
+		
+		if(type.equals(TYPE_TEXT_FIELD)){
+			
+			JTextField text = (JTextField)component;
+			text.addKeyListener(listener);
+			
+		}else if(type.equals(TYPE_TEXT_AREA)){
+			
+			JTextArea text = (JTextArea)component;
+			text.addKeyListener(listener);
+			
+		}
+			
+	}
+	
+	
+	/**
+	 * Creates a JComboBox splitting the options field by , to get the items
+	 * If name is set, makes a JLabel to go with it
+	 */
+	private void createCombo() {
+		
+		String[] itemsArray = options.split(","); //$NON-NLS-1$
+		Vector<String> items = new Vector<String>(itemsArray.length);
+		
+		JComboBox<String> combo = new JComboBox<String>(items);
+		combo.setEditable(false);
+		
+		if(name != null && name.length() > 0){
+			
+			label = new JLabel(name);
+			label.setLabelFor(combo);
+			if(tooltip != null && tooltip.length() > 0)
+				label.setToolTipText(tooltip);
+			
+		}
+		
+		component = combo;
+		
+	}
+
+	/**
+	 * Creates a JTextArea enclosed by a JScrollPane
+	 * Uses name to create a label associated with this
+	 */
+	private void createTextArea() {
+		
+		JTextArea text = new JTextArea();
+		
+		if(name != null && name.length() > 0){
+			
+			label = new JLabel(name);
+			label.setLabelFor(text);
+			if(tooltip != null && tooltip.length() > 0)
+				label.setToolTipText(tooltip);
+			
+		}
+		
+		if(tooltip != null && tooltip.length() > 0)
+			text.setToolTipText(tooltip);
+		
+		text.setRows(3);
+		text.setWrapStyleWord(true);
+		text.setLineWrap(true);
+		JScrollPane scroll = new JScrollPane(text);
+		
+		component = scroll;
+		
+	}
+
+	/**
+	 * Creates an empty JTextField
+	 * Uses name to create a label for this field
+	 */
+	private void createTextField() {
+		
+		JTextField text = new JTextField();
+		
+		if(name != null && name.length() > 0){
+			
+			label = new JLabel(name);
+			label.setLabelFor(text);
+			if(tooltip != null && tooltip.length() > 0)
+				label.setToolTipText(tooltip);
+			
+		}
+		
+		if(tooltip != null && tooltip.length() > 0)
+			text.setToolTipText(tooltip);
+		
+		component = text;
+		
+	}
+
+	/**
+	 * Creates a JLabel using the name field
+	 */
+	private void createLabel() {
+		
+		JLabel label = new JLabel(name);
+		
+		if(tooltip != null && tooltip.length() > 0)
+			label.setToolTipText(tooltip);
+		
+		component = label;
+		
+	}
+	
+}
---
+++ src/com/thevoiceasia/phonebox/callinput/CallInputPanel.java
@@ -0,0 +1,135 @@
+package com.thevoiceasia.phonebox.callinput;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Vector;
+import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTabbedPane;
+
+public class CallInputPanel extends JTabbedPane {
+
+	/* CLASS VARS */
+	private String language, country;
+	private Connection databaseConnection;
+	private Vector<CallInputField> components = new Vector<CallInputField>();
+	private I18NStrings xStrings;
+	private boolean hasErrors = false;
+	
+	/** STATICS **/
+	private static final long serialVersionUID = 1L;
+	private static final Logger LOGGER = Logger.getLogger(CallInputPanel.class.getName());//Logger
+	
+	public CallInputPanel(Connection readConnection, String language, String country) {
+		
+		super(JTabbedPane.RIGHT);
+		
+		xStrings = new I18NStrings(language, country);
+		
+		//Read components from DB
+		if(getComponentDetails())
+			//Create the JTabbedPane
+			createTabbedPane();
+		else
+			hasErrors = true;
+		
+	}
+	
+	/**
+	 * Flag to show
+	 * @return
+	 */
+	public boolean hasErrors(){
+		
+		return hasErrors;
+		
+	}
+	
+	/**
+	 * Creates the actual GUI
+	 */
+	private void createTabbedPane(){
+		
+		//TODO
+		
+	}
+	
+	/**
+	 * Reads the component info from the DB
+	 * @return
+	 */
+	private boolean getComponentDetails(){
+		
+		Statement statement = null;
+		ResultSet resultSet = null;
+		boolean gotSettings = false;
+		
+		try{
+			
+			String SQL = "SELECT * FROM callinputfields WHERE language = '" + language +  //$NON-NLS-1$
+					"," + country + "' ORDER BY order ASC"; //$NON-NLS-1$ //$NON-NLS-2$
+			
+			statement = databaseConnection.createStatement();
+		    resultSet = statement.executeQuery(SQL);
+		    
+		    while(resultSet.next()){
+		    	
+		    	//int id, String name, String type, String tooltip, int order, 
+				//int parent, String options
+		    	components.add(new CallInputField(resultSet.getInt("id"), //$NON-NLS-1$
+		    			resultSet.getString("name"), //$NON-NLS-1$
+		    			resultSet.getString("type"), //$NON-NLS-1$
+		    			resultSet.getString("tooltip"), //$NON-NLS-1$
+		    			resultSet.getInt("order"), //$NON-NLS-1$
+		    			resultSet.getInt("parent"), //$NON-NLS-1$
+		    			resultSet.getString("options"))); //$NON-NLS-1$
+		    	
+		    	gotSettings = true;
+		    	
+		    }
+		    
+		}catch (SQLException e){
+			showError(e, xStrings.getString("CallInputPanel.getComponentsSQLError")); //$NON-NLS-1$
+		}finally {
+		    
+			if (resultSet != null) {
+		        try {
+		        	resultSet.close();
+		        } catch (SQLException sqlEx) { } // ignore
+		        resultSet = null;
+		    }
+			
+		    if (statement != null) {
+		        try {
+		        	statement.close();
+		        } catch (SQLException sqlEx) { } // ignore
+		        statement = null;
+		    }
+		    
+		}
+		
+		return gotSettings;
+		
+	}
+	
+	/**
+	 * Logs an error message and displays friendly message to user
+	 * @param e
+	 * @param friendlyErrorMessage
+	 */
+	private void showError(Exception e, String friendlyErrorMessage){
+		
+		System.err.println(xStrings.getString("CallInputPanel.logErrorPrefix") + //$NON-NLS-1$
+				friendlyErrorMessage); 
+		e.printStackTrace();
+		JOptionPane.showMessageDialog(null, friendlyErrorMessage, 
+				xStrings.getString("CallInputPanel.errorBoxTitle"), //$NON-NLS-1$
+				JOptionPane.ERROR_MESSAGE); 
+		LOGGER.severe(friendlyErrorMessage);
+		
+	}
+
+}
