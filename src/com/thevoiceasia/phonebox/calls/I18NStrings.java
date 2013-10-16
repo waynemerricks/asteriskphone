9fdb7da66f9cf7c1921deded01869f7f665d5eaa
Wayne Merricks <waynemerricks@thevoiceasia.com>
2013-10-16 18:24:19 +0100

    Added getLocale support

--- src/com/thevoiceasia/phonebox/calls/I18NStrings.java
+++ src/com/thevoiceasia/phonebox/calls/I18NStrings.java
@@ -8,14 +8,27 @@ public class I18NStrings {
 	
 	private static final String BUNDLE_NAME = "com.thevoiceasia.phonebox.calls.strings"; //$NON-NLS-1$
 	private ResourceBundle RESOURCE_BUNDLE;
+	private String language, country;
 	
 	public I18NStrings(String language, String country){
 		
+		this.language = language;
+		this.country = country;
 		Locale currentLocale = new Locale(language, country);
 		RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, currentLocale);
 		
 	}
 
+	/**
+	 * Returns the locale this is using in a comma separated list
+	 * E.g. en GB becomes en,GB
+	 * @return
+	 */
+	public String getLocale(){
+		return language + "," + country; //$NON-NLS-1$
+	}
+	
+	
 	public String getString(String key) {
 		try {
 			return RESOURCE_BUNDLE.getString(key);
