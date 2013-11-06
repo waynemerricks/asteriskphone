32377c07022c071d233f06e3ddd376a7508a076d
Wayne Merricks <waynemerricks@thevoiceasia.com>
2013-11-06 14:32:45 +0000

    BUG FIX: Moved to JTextPane to get around horrible JTextArea border issues Removed highlighting/focus border changes as cell resized in odd ways

--- src/com/thevoiceasia/phonebox/callinput/MultiLineCellRenderer.java
+++ src/com/thevoiceasia/phonebox/callinput/MultiLineCellRenderer.java
@@ -4,11 +4,10 @@ import java.awt.Component;
 import java.awt.Dimension;
 
 import javax.swing.JTable;
-import javax.swing.JTextArea;
-import javax.swing.UIManager;
+import javax.swing.JTextPane;
 import javax.swing.table.TableCellRenderer;
 
-public class MultiLineCellRenderer extends JTextArea implements
+public class MultiLineCellRenderer extends JTextPane implements
 		TableCellRenderer {
 
 	/** STATICS **/
@@ -16,34 +15,14 @@ public class MultiLineCellRenderer extends JTextArea implements
 
 	public MultiLineCellRenderer() {
 		
-		this.setLineWrap(true);
-		this.setWrapStyleWord(true);
+		//this.setLineWrap(true);
+		//this.setWrapStyleWord(true);
 		
 	}
 
 	@Override
 	public Component getTableCellRendererComponent(JTable table, Object value,
 			boolean isSelected, boolean hasFocus, int row, int column) {
-		// TODO Auto-generated method stub
-		
-		//Set the colours to match the normal table selection colours
-		if(isSelected){
-			
-			setForeground(table.getSelectionForeground());
-			setBackground(table.getSelectionBackground());
-			
-		}else{//Not Selected
-			
-		    setForeground(table.getForeground());
-		    setBackground(table.getBackground());
-		    
-		}
-		
-		//Set the colours to match normal table focus colours
-		if(hasFocus)
-			setBorder(UIManager.getBorder("Table.focusCellHighlightBorder")); //$NON-NLS-1$
-		else
-			setBorder(UIManager.getBorder("Table.CellBorder")); //$NON-NLS-1$
 		
 		//Set the text of this object to the Object value
 		if(value != null)
