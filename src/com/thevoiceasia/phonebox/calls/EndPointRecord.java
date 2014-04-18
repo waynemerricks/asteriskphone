d1a54772a3e203b63cfed6cb6088d48769d6a80c
Wayne Merricks <waynemerricks@thevoiceasia.com>
2014-04-18 17:49:58 +0100

    Added toString for debugging purposes

--- src/com/thevoiceasia/phonebox/calls/EndPointRecord.java
+++ src/com/thevoiceasia/phonebox/calls/EndPointRecord.java
@@ -12,4 +12,11 @@ public class EndPointRecord {
 		
 	}
 
+	public String toString(){
+		
+		return "ENDPOINT RECORD:\n\tCaller Channel: " + callerChannel  //$NON-NLS-1$
+				+ "\n\tReceiver Channel: " + receiverChannel //$NON-NLS-1$
+				+ "\n\tReceiver Caller ID: " + receiverCallerID + "\n";  //$NON-NLS-1$//$NON-NLS-2$
+		
+	}
 }
