d3ae9de2c760deef5fd4251a01bc65375eaecdf7
Wayne Merricks <waynemerricks@thevoiceasia.com>
2014-06-20 15:09:24 +0100

    Removed: isEmpty & setEmpty no longer required Added: Comments for javadocs Added: void clear() - Clears the search term to null

--- src/com/thevoiceasia/phonebox/callinput/SearchTerm.java
+++ src/com/thevoiceasia/phonebox/callinput/SearchTerm.java
@@ -2,9 +2,14 @@ package com.thevoiceasia.phonebox.callinput;
 
 public class SearchTerm {
 
-	private boolean number = false, queueNumber = false, empty = true, locked = false;
+	private boolean number = false, queueNumber = false, locked = false;
 	private String term = null, queueTerm = null;
 	
+	/**
+	 * Creates a search term
+	 * @param numberMode
+	 * @param term
+	 */
 	public SearchTerm(boolean numberMode, String term) {
 		
 		this.number = numberMode;
@@ -12,12 +17,30 @@ public class SearchTerm {
 		
 	}
 	
+	/**
+	 * Checks if this term is locked due to being queried
+	 * @return
+	 */
 	public synchronized boolean isLocked(){
 		
 		return locked;
 		
 	}
 	
+	/**
+	 * Clears the search term to null
+	 */
+	public synchronized void clear(){
+		
+		term = null;
+		
+	}
+	
+	/**
+	 * Sets the thread locked, if unlocked it will also move
+	 * any queued terms to the main terms before unlocking
+	 * @param lock
+	 */
 	public synchronized void setLocked(boolean lock){
 		
 		locked = lock;
@@ -34,30 +57,31 @@ public class SearchTerm {
 		
 	}
 	
-	public boolean isEmpty(){
-		
-		return empty;
-		
-	}
-	
-	public void setEmpty(boolean yes){
-		
-		empty = yes;
-		
-	}
-	
+	/**
+	 * Returns whether the search term is a number (true) or name (false)
+	 * @return
+	 */
 	public synchronized boolean isNumber(){
 		
 		return number;
 		
 	}
 	
+	/**
+	 * Returns the search term
+	 * @return
+	 */
 	public synchronized String getSearchTerm(){
 		
 		return term;
 		
 	}
 	
+	/**
+	 * Changes the mode/term to a new mode/term
+	 * @param numberMode
+	 * @param term
+	 */
 	public synchronized void updateSearch(boolean numberMode, String term){
 		
 		this.number = numberMode;
@@ -65,6 +89,11 @@ public class SearchTerm {
 		
 	}
 	
+	/**
+	 * Changes the queued mode/term to a new mode/term
+	 * @param numberMode
+	 * @param term
+	 */
 	public synchronized void queueSearch(boolean numberMode, String term){
 		
 		this.queueNumber = numberMode;
