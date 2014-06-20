5f301e5fea6a35fefbdefc39b10762c866f7f7d0
Wayne Merricks <waynemerricks@thevoiceasia.com>
2014-06-20 13:59:33 +0100

    Thread that acts on the SearchTerm at a timed interval (currently every 500ms)

---
+++ src/com/thevoiceasia/phonebox/callinput/SearchPersonThread.java
@@ -0,0 +1,43 @@
+package com.thevoiceasia.phonebox.callinput;
+
+public class SearchPersonThread implements Runnable {
+
+	private boolean go = true;
+	private SearchPanel search = null;
+	
+	private static final long SEARCH_INTERVAL = 500l;
+	
+	public SearchPersonThread(SearchPanel search) {
+		
+		this.search = search;
+		
+	}
+
+	@Override
+	public void run() {
+		
+		while(go){
+			
+			try {
+				
+				//Fire an update check on the SearchPanel
+				search.checkDataNeedsUpdating();
+				//Wait for 500ms to prevent db query spam
+				Thread.sleep(SEARCH_INTERVAL);
+				
+			} catch (InterruptedException e) {
+				
+				go = false;
+				
+			}
+			
+		}
+	}
+	
+	public void stopMeGracefully(){
+		
+		go = false;
+		
+	}
+
+}
