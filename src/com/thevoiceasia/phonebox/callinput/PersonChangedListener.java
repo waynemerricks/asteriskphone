f993032856bb8084a2f2c912d6f25bd5faef20c4
Wayne Merricks <waynemerricks@thevoiceasia.com>
2014-07-28 14:06:25 +0100

    Added JavaDocs for personChanged(Person)

--- src/com/thevoiceasia/phonebox/callinput/PersonChangedListener.java
+++ src/com/thevoiceasia/phonebox/callinput/PersonChangedListener.java
@@ -4,6 +4,12 @@ import com.thevoiceasia.phonebox.records.Person;
 
 public interface PersonChangedListener {
 
+	/**
+	 * Notifies the object that implements this interface that the Person object it references
+	 * has been changed to a different Person
+	 * @param changedTo null = need to create a new person, otherwise swap it to the referenced
+	 * Person
+	 */
 	public void personChanged(Person changedTo);
 	
 }
